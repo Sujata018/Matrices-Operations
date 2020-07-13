250ee657d42b6fefaea0d7caccf411f92e3b3072
Sujata018 <60694602+Sujata018@users.noreply.github.com>
2020-07-13 20:45:33 +0530

    Update CalculateDeterminant.py

--- CalculateDeterminant.py
+++ CalculateDeterminant.py
@@ -1,44 +1,11 @@
-# This function calcultes the determinant of any sqauare matrix
-'''
+# This function calcultes the determinant of any sqauare matrix of any dimension
+#
+# Enter your matrix in list form in lines 32, like below, and run the program to get determinant.
+# matrix = [[12,4,3],[8,-12,-9],[33,-4,-24]]
+##
+
 import math
 
-order = 4
-    
-a1 = [1,2,3,5]
-a2 = [1,4,9,25]
-
-a3 = [1,8,27,125]
-a4 = [1,16,81,625]
-
-
-c0=(1+math.sqrt(3))/4*math.sqrt(2)
-c1=(3+math.sqrt(3))/4*math.sqrt(2)
-c2=(3-math.sqrt(3))/4*math.sqrt(2)
-c3=(1-math.sqrt(3))/4*math.sqrt(2)
-a1 = [c0,c1,c2,c3]
-a2 = [c2,c3,c0,c1]
-a3 = [c3,-c2,c1,-c0]
-a4 = [c1,-c0,c3,-c2]
-
-
-result = ( a1[0]*(  a2[1] * (a3[2]*a4[3] - a3[3]*a4[2])
-                  - a3[1] * (a2[2]*a4[3] - a2[3]*a4[2])
-                  + a4[1] * (a2[2]*a3[3] - a2[3]*a3[2])
-                  )
-          -a2[0]*(  a1[1] * (a3[2]*a4[3] - a3[3]*a4[2])
-                  - a3[1] * (a1[2]*a4[3] - a1[3]*a4[2])
-                  + a4[1] * (a1[2]*a3[3] - a1[3]*a3[2])
-                  )
-          +a3[0]*(  a1[1] * (a2[2]*a4[3] - a2[3]*a4[2])
-                  - a2[1] * (a1[2]*a4[3] - a1[3]*a4[2])
-                  + a4[1] * (a1[2]*a2[3] - a1[3]*a2[2])
-                 )
-          -a4[0]*(  a1[1] * (a2[2]*a3[3] - a2[3]*a3[2])
-                  - a2[1] * (a1[2]*a3[3] - a1[3]*a3[2])
-                  + a3[1] * (a1[2]*a2[3] - a1[3]*a2[2])
-                 )
-         )
-'''
 def getCofactor(rowPred, CurCol):   # Get cofactor for the current row and column
                                     # rowPred contains the list of row numbers for the hiarrchy                                   
     maxindex = Dimension - 1
@@ -62,8 +29,8 @@ def getCofactor(rowPred, CurCol):   # Get cofactor for the current row and colum
                     result += (-1) * matrix[i][CurCol] * cof
     return result
 
-#matrix = [[2,4,8,16],[3,9,27,81],[1,1,1,1],[5,25,125,625]]
-#matrix = [[12,4,3],[8,-12,-9],[33,-4,-24]]
+#matrix = [[2,4,8,16],[3,9,27,81],[1,1,1,1],[5,25,125,625]]   # 4 x 4 matrix structure
+matrix = [[12,4,3],[8,-12,-9],[33,-4,-24]]    # 3 x 3 matrix structure
 Dimension = len(matrix)
 result = getCofactor([], 0)
 
